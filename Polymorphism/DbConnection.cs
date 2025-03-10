483ddf27c44a2fa386516a18e384f892ec62313f
taofeeekishola <50875900+taofeeekishola@users.noreply.github.com>
2025-03-10 11:19:25 +0100

    Add files via upload

---
+++ Polymorphism/DbConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public abstract class DbConnection
+    {
+        public  string ConnectionString { get; private set; }
+
+        public TimeSpan Timeout { get; set; }
+
+        public DbConnection(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+            {
+                throw new InvalidOperationException("Invalid input");
+            }
+
+            ConnectionString = connectionString;
+            Timeout = TimeSpan.FromSeconds(30); // Default timeout
+        }
+
+        public abstract void open(); // Abstract method to be implemented by derived classes
+        public abstract void close(); 
+
+
+
+    }
+}
+
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Polymorphism;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DbConnection sqlConnect = new SqlConnection("Server=myServer;Database=myDB;");
+        sqlConnect.open();
+        sqlConnect.close();
+
+        Console.WriteLine();
+
+        DbConnection oracleConnect = new OracleConnection("Host=myOracleServer;DB=myOracleDB;");
+        oracleConnect.open();
+        oracleConnect.close();
+
+        // Uncommenting the following will throw an exception:
+        //DbConnection invalidConnect = new SqlConnection("");
+    }
+
+}
+
